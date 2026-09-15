98584dca761f938a67a21fb340c289918843df88
ronaldsanchezrr3-ship-it <ronaldsanchezrr3@gmail.com>
2026-09-15 12:33:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const crypto = require("crypto");
 const app = express();
 
 app.use(cors({
-  origin: "https://ecoll-ins.onrender.com"
+  origin: "https://ecocashlo-an.onrender.com"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
